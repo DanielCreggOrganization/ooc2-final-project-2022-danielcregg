a2113087574ccd6817d9a4c259fe1e453726cd4e
danielcregg <daniel.cregg@atu.ie>
2022-11-21 17:24:19 +0000

    Added getters and setters

--- src/ie/atu/bookmanagerpackage/Book.java
+++ src/ie/atu/bookmanagerpackage/Book.java
@@ -7,5 +7,39 @@ public class Book {
 	private String author;
 	private int age;
 	private String title;
+	
+	// Getters and Setters
+	public String getBookId() {
+		return bookId;
+	}
+	
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
 
 }
