1fd5fa214d39fee1885d4cca54d50d01657f95b2
danielcregg <daniel.cregg@atu.ie>
2022-11-21 17:22:42 +0000

    Added book class

--- src/ie/atu/bookmanagerpackage/Book.java
+++ src/ie/atu/bookmanagerpackage/Book.java
@@ -1,5 +1,11 @@
 package ie.atu.bookmanagerpackage;
 
 public class Book {
+	
+	// Instance Variables
+	private String bookId;
+	private String author;
+	private int age;
+	private String title;
 
 }
